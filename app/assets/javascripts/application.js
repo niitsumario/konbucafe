c4c1fbb59a8b23fc4d509c6afdb37f1f06568587
niitsumario <xxx.ballet121.xxx@gmail.com>
2021-07-11 13:43:48 +0000

    トップページの作成

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,17 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+jQuery(function ($) {
+  var fadeIn = $('.fade-in');
+  $(window).on('scroll', function () {
+    $(fadeIn).each(function () {
+      var offset = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > offset - windowHeight + 150) {
+        $(this).addClass("scroll-in");
+      }
+    });
+  });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'homes#top'
+  get 'homes/about' => 'homes#about', as: 'about'
 end
