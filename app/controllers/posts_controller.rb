866624606f11532c3027256a83aac873b4dc5116
niitsumario <xxx.ballet121.xxx@gmail.com>
2021-07-13 08:30:58 +0000

    post_show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 
   def index
     @post_new = Post.new
-    @post_new.build_spot
+    #@post_new.build_spot
     @posts = Post.all
   end
 
@@ -12,16 +12,17 @@ class PostsController < ApplicationController
     if @post_new.save
       redirect_to posts_path
     else
-      redirect_to posts_path
+      redirect_to :index
     end
   end
 
   def show
+    @post_new = Post.new
     @post = Post.find(params[:id])
-    @lat = @post.spot.latitude
-    @lng = @post.spot.longitude
-    gon.lat = @lat
-    gon.lng = @lng
+    #@lat = @post.spot.latitude
+    #@lng = @post.spot.longitude
+    #gon.lat = @lat
+    #gon.lng = @lng
   end
 
   def edit
@@ -31,12 +32,18 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to posts_path
+    else
+      render :show
+    end
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :image, :shop_name, :introduction, spot_attributes: [:address])
+    params.require(:post).permit(:title, :image, :shop_name, :introduction)#, spot_attributes: [:address])
   end
 
 end
