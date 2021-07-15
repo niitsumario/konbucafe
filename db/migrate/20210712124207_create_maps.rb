c7a77ddcf23052efea91939007f5c8f20ff38aef
niitsumario <xxx.ballet121.xxx@gmail.com>
2021-07-15 10:10:18 +0000

    fixed migrate

--- db/migrate/20210712124207_create_maps.rb
+++ db/migrate/20210712124207_create_maps.rb
@@ -4,7 +4,7 @@ class CreateMaps < ActiveRecord::Migration[5.2]
       t.string :address
       t.float :latitude
       t.float :longitude
-      t.references :review_id, foreign_key: true
+#      t.references :review_id, foreign_key: true
 
       t.timestamps
     end
